7e8f3ccaa06b2758ce13c1fc585089ca297b4035
(^_^) <84281082+DmitryShetinin@users.noreply.github.com>
2021-10-19 11:17:48 +0300

    lab_2

---
+++ ConsoleApp1/Solve_x1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Solve_x1
+    {
+
+        protected List<float> X;
+        public List<float> solve_x1(float a, float b)
+        {
+
+            if (a == 0)
+                return null;
+
+
+            return X = new List<float>() { -b / a };
+            //        X.Add(-b / a);
+
+            //      return X; 
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Solve_x2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Solve_x2 : Solve_x1
+    {
+        protected float disc(float a, float b, float c)
+        {
+            return b * b - (4 * a * c);
+        }
+
+        public List<float> solve_x2(float a, float b, float c)
+        {
+
+            if (a == 0)
+            {
+                return solve_x1(b, c);
+            }
+
+
+            float D = disc(a, b, c);
+
+            if (D < 0)
+            {
+                return null;
+            }
+
+            //list = new List<double>(2);
+            if (D == 0)
+                return X = new List<float>() { (-b) / (2 * a) };
+
+            D = (float)Math.Sqrt(D);//-b/(2a) - D/(2a)
+            //c = 
+            return X = new List<float>() {
+                (-b + D) / (2 * a),
+                (-b - D) / (2 * a)
+            };
+
+        }
+    }
+}
